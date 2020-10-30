92cc9d0fd11e391e350b2a8e257e69f52e0da3cc
ustyuzhaninky <ustyuzhanin.ky@gmail.com>
2020-10-30 19:09:57 +0500

    transformer-xl compatability files

---
+++ OSAR/tfxl/__init__.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+# Copyright 2020 Konstantin Ustyuzhanin.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+name = 'tfxl'
+
+from .scale import *
+from .memory import *
+from .pos_embed import *
+from .rel_bias import *
+from .rel_multi_head import *
+from .loader import *
+from .transformer_xl import *
+from .sequence import *
+from .softmax import *
+from .embedding import *
+from .feed_forward import *
+from .layer_normalization import *
