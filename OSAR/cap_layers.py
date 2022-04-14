Explosive gradient fix

OSAR/cap_layers.py
@@ -77,12 +77,26 @@ class Capsule(Layer):
                  routings=3,
                  share_weights=True,
                  activation='squash',
+                 kernel_initializer='glorot_normal',
+                 kernel_regularizer=None,
+                 kernel_constraint=None,
+                 bias_initializer='glorot_normal',
+                 bias_regularizer=None,
+                 bias_constraint=None,
                  **kwargs):
         super(Capsule, self).__init__(**kwargs)
         self.num_capsule = num_capsule
         self.dim_capsule = dim_capsule
         self.routings = routings
         self.share_weights = share_weights
+
+        self.kernel_initializer = tf.keras.initializers.get(kernel_initializer)
+        self.kernel_regularizer = tf.keras.regularizers.get(kernel_regularizer)
+        self.kernel_constraint = tf.keras.constraints.get(kernel_constraint)
+        self.bias_initializer = tf.keras.initializers.get(bias_initializer)
+        self.bias_regularizer = tf.keras.regularizers.get(bias_regularizer)
+        self.bias_constraint = tf.keras.constraints.get(bias_constraint)
+
         if activation == 'squash':
             self.activation = squash
         else:
@@ -95,7 +109,9 @@ class Capsule(Layer):
                 name=f'{self.name}-capsule-kernel',
                 shape=(1, input_dim_capsule,
                        self.num_capsule * self.dim_capsule),
-                initializer='glorot_uniform',
+                initializer=self.kernel_initializer,
+                regularizer=self.kernel_regularizer,
+                constraint=self.kernel_constraint,
                 trainable=True)
         else:
             input_num_capsule = input_shape[-2]
@@ -103,7 +119,9 @@ class Capsule(Layer):
                 name=f'{self.name}-capsule-kernel',
                 shape=(input_num_capsule, input_dim_capsule,
                        self.num_capsule * self.dim_capsule),
-                initializer='glorot_uniform',
+                initializer=self.kernel_initializer,
+                regularizer=self.kernel_regularizer,
+                constraint=self.kernel_constraint,
                 trainable=True)
 
     def call(self, inputs):
@@ -167,7 +185,7 @@ class Capsule1D(Layer):
             self.W = self.add_weight(name=f'{self.name}-capsule-kernel',
                                      shape=(1, input_dim_capsule,
                                             self.num_capsule * self.dim_capsule),
-                                     initializer='glorot_uniform',
+                                     initializer='glorot_normal',
                                      trainable=True)
         else:
             input_num_capsule = input_shape[-2]
@@ -175,7 +193,7 @@ class Capsule1D(Layer):
                                      shape=(input_num_capsule,
                                             input_dim_capsule,
                                             self.num_capsule * self.dim_capsule),
-                                     initializer='glorot_uniform',
+                                     initializer='glorot_normal',
                                      trainable=True)
 
     def call(self, u_vecs):

OSAR/graph_attention.py
@@ -50,10 +50,10 @@ class GraphAttention(tf.keras.layers.Layer):
 
     def __init__(self,
                  units,
-                 activation='sigmoid',
+                 activation='hard_sigmoid',
                  use_bias=True,
                  attention_dropout=0.0,
-                 kernel_initializer='glorot_normal',
+                 kernel_initializer='he_normal',
                  bias_initializer='zeros',
                  kernel_regularizer=None,
                  bias_regularizer=None,
@@ -87,7 +87,7 @@ class GraphAttention(tf.keras.layers.Layer):
         
         self.kernel = self.add_weight(
             shape=(2 * feature_dim,),
-            initializer=self.kernel_initializer,
+            initializer=tf.keras.initializers.HeNormal(),
             regularizer=self.kernel_regularizer,
             constraint=self.kernel_constraint,
             name='kernel',
@@ -96,7 +96,7 @@ class GraphAttention(tf.keras.layers.Layer):
         if self.use_bias:
             self.bias = self.add_weight(
                 shape=(2 * feature_dim,),
-                initializer=self.bias_initializer,
+                initializer=tf.keras.initializers.HeNormal(),
                 regularizer=self.bias_regularizer,
                 constraint=self.bias_constraint,
                 name='bias',

OSAR/graph_memory.py
@@ -77,10 +77,10 @@ class EventSpace(tf.keras.layers.Layer):
         shape:Iterable,
         attention_dropout=0.0,
         use_bias:bool=True,
-        kernel_initializer='glorot_uniform',
+        kernel_initializer='glorot_normal',
         kernel_regularizer=None,
         kernel_constraint=None,
-        bias_initializer='glorot_uniform',
+        bias_initializer='glorot_normal',
         bias_regularizer=None,
         bias_constraint=None,
         return_space:bool=False,
@@ -149,7 +149,11 @@ class EventSpace(tf.keras.layers.Layer):
         self.caps = Capsule(
             self.units,
             self.units,
-            name=f'{self.name}_Capsule'
+            name=f'{self.name}_Capsule',
+            kernel_regularizer=self.kernel_regularizer,
+            bias_regularizer=self.bias_regularizer,
+            kernel_constraint=self.kernel_constraint,
+            bias_constraint=self.bias_constraint
         )
         self.caps.build((batch_size, seq_dim, output_dim))
         self.caps.built = True
@@ -166,7 +170,11 @@ class EventSpace(tf.keras.layers.Layer):
         self.encoder = SequenceEncoder1D(
             output_dim, seq_dim,
             activation='relu',
-            name=f'{self.name}_encoder'
+            name=f'{self.name}_encoder',
+            kernel_regularizer=self.kernel_regularizer,
+            bias_regularizer=self.bias_regularizer,
+            kernel_constraint=self.kernel_constraint,
+            bias_constraint=self.bias_constraint
             )
         self.encoder.build(
             (batch_size, self.units, self.units))
@@ -178,7 +186,7 @@ class EventSpace(tf.keras.layers.Layer):
         #     name=f'{self.name}_scaler'
         #     )
         # self.scaler.build(
-        #     (batch_size, output_dim*output_dim, seq_dim*seq_dim))#self.units, self.units))
+        #     (batch_size, self.units, self.units))
         # self.scaler.built = True
                                   
         self.built = True

OSAR/helix_memory.py
@@ -62,7 +62,7 @@ class HelixMemory(tf.keras.layers.Layer):
         n_turns,
         compression_rate=2,
         mode='avg',
-        initializer='glorot_uniform',
+        initializer='glorot_normal',
         regularizer='l2',
         constraint=None,
         **kwargs):
@@ -98,7 +98,7 @@ class HelixMemory(tf.keras.layers.Layer):
         self.memory = self.add_weight(
             shape=(self.memory_len +
                    n_conv, output_dim),
-            initializer='glorot_uniform',
+            initializer='glorot_normal',
             trainable=False,
             name=f'{self.name}-memory',
         )

OSAR/network/osar_distribution_network.py
@@ -95,7 +95,7 @@ class OSARActorDistributionNetwork(actor_distribution_network.ActorDistributionN
                 have the same length of fc_layer_params, or be None.
             activation_fn: Activation function, e.g. tf.nn.relu, slim.leaky_relu, ...
             kernel_initializer: Initializer to use for the kernels of the conv and
-                dense layers. If none is provided a default glorot_uniform.
+                dense layers. If none is provided a default glorot_normal.
             seed_stream_class: The seed stream class. This is almost always
                 tfp.util.SeedStream, except for in unit testing, when one may want to
                 seed all the layers deterministically.

OSAR/queue_memory.py
@@ -55,7 +55,7 @@ class QueueMemory(tf.keras.layers.Layer):
         self,
         memory_len: int,
         epsilon_probability: float = 0.51,
-        kernel_initializer = 'glorot_uniform',
+        kernel_initializer = 'glorot_normal',
         kernel_regularizer = None,
         kernel_constraint = None,
         **kwargs):
@@ -81,13 +81,17 @@ class QueueMemory(tf.keras.layers.Layer):
 
         self.memory = self.add_weight(
             shape=(self.memory_len, feature_dim),
-            initializer='glorot_uniform',
+            initializer=tf.keras.initializers.GlorotNormal(),#self.kernel_initializer,
+            # regularizer=self.kernel_regularizer,
+            # constraint=self.kernel_constraint,
             trainable=False,
             name=f'{self.name}_memory',
         )
         self.index = self.add_weight(
             shape=(self.memory_len, 1),
-            initializer='glorot_uniform',
+            initializer=tf.keras.initializers.GlorotNormal(),#self.kernel_initializer,
+            # regularizer=self.kernel_regularizer,
+            constraint=self.kernel_constraint,
             trainable=False,
             name=f'{self.name}_index',
         )
@@ -97,7 +101,7 @@ class QueueMemory(tf.keras.layers.Layer):
     def _compute_compatability(self, logit, source):
         """Computes a compatability for a single-shot """
         levels = source - logit
-        levels = 0.5 - K.hard_sigmoid(levels)
+        levels = tf.raw_ops.LeakyRelu(features=levels)
         return levels
 
     def _remove_add_by_index(self, index, new_index, new_memory):

OSAR/sympathetic_circuit.py
@@ -69,7 +69,7 @@ class SympatheticCircuit(tf.keras.layers.Layer):
                  memory_len,
                  dropout=0.0,
                  use_bias=True,
-                 kernel_initializer='glorot_uniform',
+                 kernel_initializer='glorot_normal',
                  kernel_regularizer=None,
                  kernel_constraint=None,
                  bias_initializer='truncated_normal',